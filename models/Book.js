19c6220df94566158793847ffb74bb203ee0b067
Matthew Jones <mejones92@gmail.com>
2021-05-25 21:47:18 +0100

    Updated mongodb model for the Book ORM.

--- models/Book.js
+++ models/Book.js
@@ -2,10 +2,12 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
+  bookId: { type: String, required: true, unique: true },
   title: { type: String, required: true },
-  author: { type: String, required: true },
+  authors: { type: String, required: true },
   description: { type: String, required: true },
   image: { type: String, required: true },
+  link: { type: String, required: true },
 });
 
 const Book = mongoose.model("Book", bookSchema);

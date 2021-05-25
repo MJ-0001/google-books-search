0c9f3bff491a75cb618eb1b4868f5181092945dd
Matthew Jones <mejones92@gmail.com>
2021-05-25 22:30:45 +0100

    Added client/build static path.

--- routes/api/api-index.js
+++ routes/api/api-index.js
@@ -7,7 +7,7 @@ router.use("/book", bookRoutes);
 
 // For anything else, render the html page
 router.use(function(req, res) {
-  res.sendFile(path.join(__dirname, "../../client/public/index.html"));
+  res.sendFile(path.join(__dirname, "../../client/build/index.html"));
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/public"));
+  app.use(express.static("client/build"));
 }
 
 // Add routes, both API and view

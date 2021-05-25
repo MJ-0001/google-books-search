22fee8235c95d94777e2c2bafc1c40e33b939ee4
Matthew Jones <mejones92@gmail.com>
2021-05-25 21:51:56 +0100

    Updated mongodb CRUD functions.

---
+++ controllers/booksController.js
@@ -0,0 +1,30 @@
+const Book = require("../models/book");
+
+module.exports = {
+  findAll: function(req, res) {
+    Book.find(req.query)
+      .then(dbBook => res.json(dbBook))
+      .catch(err => res.status(422).json(err));
+  },
+  // findById: function(req, res) {
+  //   Book.findById(req.params.id)
+  //     .then(dbBook => res.json(dbBook))
+  //     .catch(err => res.status(422).json(err));
+  // },
+  create: function(req, res) {
+    Book.create(req.body)
+      .then(dbBook => res.json(dbBook))
+      .catch(err => res.status(422).json(err));
+  },
+  // update: function(req, res) {
+  //   Book.findOneAndUpdate({ id: req.params.id }, req.body)
+  //     .then(dbBook => res.json(dbBook))
+  //     .catch(err => res.status(422).json(err));
+  // },
+  remove: function(req, res) {
+    Book.findById(req.params.id)
+      .then(dbBook => dbBook.remove())
+      .then(dbBook => res.json(dbBook))
+      .catch(err => res.status(422).json(err));
+  }
+};

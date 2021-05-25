dfbdeb60ce8b377b5230c0682596abbbc9a69b85
Matthew Jones <mejones92@gmail.com>
2021-05-25 21:46:47 +0100

    Updated mongodb connection string.

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 const express = require("express");
-
 const mongoose = require("mongoose");
-const routes = require("./routes");
+const routes = require("./routes/routes-index");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
+const logger = require("morgan");
 
+app.use(logger("dev"));
 // Configure body parsing for AJAX requests
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("client/public"));
 }
 
 // Add routes, both API and view
@@ -18,14 +19,16 @@ app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost:27017/google-books-search",
+  process.env.MONGODB_URI || "mongodb://localhost:27017/google_book_search",
   {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
     useCreateIndex: true,
-    useNewUrlParser: true
   }
 );
 
-// Start the server
+// Start the API server
 app.listen(PORT, () =>
   console.log(`Server now listening on PORT ${PORT}!`)
 );
